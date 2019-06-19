ae21e306558c049cbe48aa1361dc382991eaead8
Damola <damolasd@gmail.com>
2019-06-19 14:10:13 +0100

    action types and creators added

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,3 +6,40 @@
 // our action creator will be a function that returns a function
 // the url to fetch characters from is `https://swapi.co/api/people/`
 // remember that now we have controll over our thunk-based action creator
+import axios from 'axios';
+import { types } from 'util';
+
+//Action Types
+export const FETCH_CHARACTERS = 'FETCH_CHARACTERS';
+export const SUCCESS = 'SUCCESS';
+export const FAILURE = 'FAILURE';
+
+//ActionCreators
+export const success = (data) => {
+    return {
+        type: types.SUCCESS,
+        payload: data,
+    }
+}
+
+
+export const failure = (message) => {
+    return {
+        type: types.FAILURE,
+        payload: data,
+    }
+}
+
+export const fetchCharacters = () => dispatch => {
+    // we code
+    axios.get('https://swapi.co/api/people/')
+      .then(res => {
+        // what do you suggest?
+        // dispatch({ type: types.ADD_QUOTES, payload: res.data }); // option 1
+        dispatch(success(res.data)); // option 2
+      })
+      .catch(error => {
+        console.log(error.message);
+        dispatch(failure(error.message));
+      });
+  };
